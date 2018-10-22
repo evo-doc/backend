c7fa0349e15992e9ff566a560a6faeef31118ecb
Daniel Hampl <danhampl31415@gmail.com>
2018-10-23 00:08:15 +0200

    going to bed please kill me

--- evodoc/api/home.py
+++ evodoc/api/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from evodoc.services.authorisation import register
 
 homeprint = Blueprint("home", __name__)
 
@@ -6,3 +7,7 @@ homeprint = Blueprint("home", __name__)
 @homeprint.route('/')
 def home():
     return jsonify("Hello there")
+
+@homeprint.route('/test')
+def test():
+    return register("kek","kek2@kek.kek","kek").serialize()
