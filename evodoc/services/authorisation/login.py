b14fb527c17f9227b64390610bdf554a57b1a241
Daniel Hampl <danhampl31415@gmail.com>
2018-10-24 10:34:58 +0200

    no user found fix

--- evodoc/services/authorisation/login.py
+++ evodoc/services/authorisation/login.py
@@ -3,7 +3,8 @@ from evodoc.exception import ApiException
 
 def login(nameOrEmail, passwd):
     user = User.query.getByNameOrEmail(nameOrEmail)
-    if user.password!=passwd: #TBA
+
+    if user==None or user.password!=passwd: #TBA
         raise ApiException(400,"Sign in data are invalid.")
     
     return user.createToken().token
