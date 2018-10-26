8c4e1fde6cd61cc75c98f0b6fc71f2ed57dc4fb0
Tomas Stary <tomas.stary@icloud.com>
2018-10-26 19:28:14 +0200

    Oprava rebase

--- evodoc/services/authorisation/register.py
+++ evodoc/services/authorisation/register.py
@@ -8,12 +8,10 @@ def register (username, email, password):
     invalid=[]
     if User.query.getByName(username,False)!=None:
         invalid.append("username")
-    
+
     if User.query.getByEmail(email,False)!=None:
         invalid.append("email")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         user=User(name=username, email=email,password=passwdHash,role_id=1)
         app.db.session.add(user)
         app.db.session.commit()
@@ -21,18 +19,11 @@ def register (username, email, password):
         # dupe_field = parse('duplicate key value violates unique constraint "{constraint}"\nDETAIL:  Key ({field})=({input}) already exists.\n', str(IntegrityError.orig))["field"]
         print(str(IntegrityError.orig))
     return user
-=======
-=======
->>>>>>> 3a87d36124975b2e832402d1629f26741887965d
+
     if invalid!=[]:
         raise DbException(400,"Sign up data are invalid or non-unique.",invalid=invalid)
 
     user=User(name=username, email=email,password=passwdHash,role_id=1)
     app.db.session.add(user)
     app.db.session.commit()
-<<<<<<< HEAD
     return user.createToken().token
->>>>>>> authentication working
-=======
-    return user.createToken().token
->>>>>>> 3a87d36124975b2e832402d1629f26741887965d
