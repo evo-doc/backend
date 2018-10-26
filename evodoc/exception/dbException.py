e87d96496d9f2581bf7e7368c581c17ce98bc8ba
Tomas Stary <tomas.stary@icloud.com>
2018-10-26 19:28:14 +0200

    Oprava rebase

--- evodoc/exception/dbException.py
+++ evodoc/exception/dbException.py
@@ -2,7 +2,6 @@ class DbException(Exception):
     """
     This exception is raised in entity module
     """
-
     def __init__(self, errorCode, message, invalid=[]):
         self.errorCode = errorCode
         self.message = message
