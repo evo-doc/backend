3fc5c0fe0b7b1d3c4216fdafd94a250789f0143a
Daniel Hampl <danhampl31415@gmail.com>
2018-11-04 02:08:21 +0100

    cancerus maximus

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,15 @@ from pytest import fixture
 
 @fixture
 def client():
-    app = evodoc.Evodoc.create_app("evodoc.testconf")
-    app.registerBlueprints()
+    app = evodoc.Evodoc.create_app()
     app.db.drop_all()
     app.db.create_all()
     client = app.test_client()
+    from evodoc.api import homeprint, auth
+    with app.app_context():
+        app.register_blueprint(homeprint)
+        app.register_blueprint(auth)
+
 
     yield client
 
