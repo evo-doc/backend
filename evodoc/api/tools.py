1623392662b2e0b2387aae011726c4555fdceb39
Tomáš Starý <tomas.stary@icloud.com>
2018-11-20 16:27:17 +0100

    Cleanup (#31)
    * removing old functions from api/tools
    
    * flake8 clenaup

--- evodoc/api/tools.py
+++ evodoc/api/tools.py
@@ -1,7 +1,4 @@
 from flask import jsonify
-from evodoc.exception import ApiException
-from evodoc.models import UserToken
-from datetime import datetime
 
 
 def serialize_list(l):
@@ -36,35 +33,3 @@ def response_ok_list(data):
         :param data: list of objects with serialize method
     """
     return jsonify(serialize_list(data))
-
-
-def validate_token(token):
-    """
-    Validate token and return its instance
-        :param token:
-    """
-    t = UserToken.query.filter_by(token=token).first()
-    if t is None:
-        raise ApiException(
-            401, "Unauthorised user (missing or outdated token)", ['token'])
-    if t.update <= datetime.utcnow():
-        t = UserToken.createSuccessor()
-    return t.token
-
-
-def validate_data(data, expected_values=[],
-                  message="Not enough data to process the request.",
-                  errorCode=422):
-    """
-    validate data by given array of keys
-    """
-    missing = []
-
-    if data is None or data == {}:
-        raise ApiException(422, "Not enough data to process the request.")
-    for value in expected_values:
-        if value not in data or data[value] is None or data[value] == {}:
-            missing.append(value)
-
-    if missing != []:
-        raise ApiException(errorCode, message, missing)
