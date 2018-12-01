237bf7e5fdbb8f961f5ccb8717e0d1917d9dd92c
Tomas Stary <tomas.stary@icloud.com>
2018-12-01 21:36:24 +0100

    user edit

--- evodoc/api/users/api_users.py
+++ evodoc/api/users/api_users.py
@@ -2,6 +2,7 @@ from evodoc.api.tools import response_ok_list, response_ok_obj
 from evodoc.api.users import users
 from evodoc.services.decorators import ValidateToken
 from evodoc.models.user import User
+from flask import g
 
 
 @users.route('', methods=['GET'])
@@ -14,3 +15,9 @@ def get_all():
 @ValidateToken()
 def get_account(username):
     return response_ok_obj(User.query.getByName(username))
+
+
+@users.route('/account', methods=['GET'])
+@ValidateToken()
+def get_my_account():
+    return response_ok_obj(g.token.user)
--- tests/integration_tests/test_users.py
+++ tests/integration_tests/test_users.py
@@ -36,6 +36,17 @@ test_get_user_error = [
     ),
 ]
 
+test_get_our_data_error = [
+    (
+        {},
+        {
+            'code': 401,
+            'invalid': ['token'],
+            'message': 'Unauthorised user (missing or outdated token)'
+        },
+    ),
+]
+
 
 @pytest.mark.parametrize('login, expected', test_get_users_error)
 def test_get_users_error(client, login, expected):
@@ -87,3 +98,34 @@ def test_get_user_ok(client):
     response = client.get('/users/testuser/account', headers=headers)
 
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize('login, expected', test_get_our_data_error)
+def test_get_our_data_error(client, login, expected):
+    headers = {}
+    if login != {}:
+        token = get_token(client, login)
+        headers = {'Authorization': 'Bearer ' + token}
+
+    response = client.get('/users/account', headers=headers)
+
+    assert response.status_code == expected['code']
+    assert response.get_json()['message'] == expected['message']
+    assert response.get_json()['invalid'] == expected['invalid']
+
+
+def test_get_our_data_ok(client):
+    token = get_token(client, {
+        'login': 'test@login.com',
+        'password': 'Test@1010'
+    })
+    headers = {'Authorization': 'Bearer ' + token}
+
+    response = client.get('/users/account', headers=headers)
+
+    assert response.status_code == 200
+    data = response.get_json()
+    assert 'name' in data
+    assert 'email' in data
+    assert 'avatar' in data
+    assert 'fullname' in data
